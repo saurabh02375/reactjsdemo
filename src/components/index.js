8854ab50cc047938447fc142a4d331d7a7780bec
Prakash Singh Basnal <72181041+Prakash0Singh@users.noreply.github.com>
2024-08-26 12:47:01 +0000

    routing

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import Home from './Home';
+import Header from './Header';
+import Register from './Register';
+
+export { Home,Header,Register }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout';
+import {Home } from './components'
 import './index.css'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      {/* <Route path='about' element={<About />} /> */}
+      {/* <Route path='contact' element={<Contact />} /> */}
+      {/* <Route path='user/:userid' element={<User />} /> */}
+    </Route>
+  )
+)
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+      <RouterProvider router={router}/>
   </StrictMode>,
+  
 )
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
