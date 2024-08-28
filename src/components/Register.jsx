498acd91149138911acbb6c5da12bcf39795f6d4
Prakash Singh Basnal <72181041+Prakash0Singh@users.noreply.github.com>
2024-08-28 10:04:15 +0000

    some-change

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -45,7 +45,6 @@ const Register = () => {
   return (
     <>
       <div className=" col-lg-6 col-12 mx-auto">
-        {/* <h1 className="text-white mt-2 mb-4 pb-2 text-center">Stay tuned!</h1> */}
         <div className="offcanvas-body d-flex flex-wrap align-items-center justify-content-center">
           <form className="register-class form" onSubmit={handleSubmit} encType="multipart/form-data">
             <p className="title">Register</p>
@@ -87,25 +86,7 @@ const Register = () => {
               />
               <span>Phone Number</span>
             </label>
-            <label className="custum-f d-flex justify-content-center" htmlFor="file">
 
-              <button className="container-btn-file">
-                <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                </svg>
-
-                Upload File
-                <input
-                  className="file"
-                  type="file"
-                  accept="image/png, image/gif, image/jpeg"
-                  id="file"
-                  name="image"
-                  onChange={handleChange}
-                />
-              </button>
-
-            </label>
             <label>
               <input
                 required
@@ -118,6 +99,27 @@ const Register = () => {
               />
               <span>Password</span>
             </label>
+
+            <label className="custum-f d-flex justify-content-center" htmlFor="file">
+
+              <button className="container-btn-file">
+                <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+
+                Upload Image
+                <input
+                  className="file"
+                  type="file"
+                  accept="image/png, image/gif, image/jpeg"
+                  id="file"
+                  name="image"
+                  onChange={handleChange}
+                />
+              </button>
+
+            </label>
+
 
             <button className="register-class submit">Submit</button>
           </form>
